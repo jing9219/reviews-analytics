cc6249f68891506a7805b5e71b6727ce94ae739d
Jing <tai292214@gmail.com>
2020-10-13 15:27:09 +0800

    add filter function

--- read.py
+++ read.py
@@ -26,3 +26,12 @@ for d in data:
 	sum_len += len(d)
 
 print('留言平均長度為', sum_len / len(data))
+
+# add篩選功能
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '比留言字數小餘100')
+print(new[0])
